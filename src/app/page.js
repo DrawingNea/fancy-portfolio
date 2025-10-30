9fec2496b84ba85b190120349b33a603b344ccbe
DrawingNea <lcy.li@outlook.de>
2025-10-30 21:59:14 +0100

    feat: add new preloader

--- src/app/page.js
+++ src/app/page.js
@@ -1,53 +1,32 @@
 'use client';
-import Image from 'next/image';
-import styles from './page.module.css';
-import { useRef, useEffect } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
+import styles from './page.module.scss';
+import { useEffect, useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import Preloader from '../components/Preloader';
+import Landing from '../components/Landing';
 
 export default function Home() {
-  const firstText = useRef(null);
-  const secondText = useRef(null);
-  const slider = useRef(null);
-  let xPercent = 0;
-  let direction = -1;
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.to(slider.current, {
-      scrollTrigger: {
-        trigger: document.documentElement,
-        scrub: 0.25,
-        start: 0,
-        end: window.innerHeight,
-        onUpdate: (e) => (direction = e.direction * -1),
-      },
-      x: '-500px',
-    });
-    requestAnimationFrame(animate);
-  }, []);
+    (async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+      const locomotiveScroll = new LocomotiveScroll();
 
-  const animate = () => {
-    if (xPercent < -100) {
-      xPercent = 0;
-    } else if (xPercent > 0) {
-      xPercent = -100;
-    }
-    gsap.set(firstText.current, { xPercent: xPercent });
-    gsap.set(secondText.current, { xPercent: xPercent });
-    requestAnimationFrame(animate);
-    xPercent += 0.1 * direction;
-  };
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = 'default';
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
+  }, []);
 
   return (
     <main className={styles.main}>
-      <Image src="/images/background.jpg" fill={true} alt="background" />
-      <div className={styles.sliderContainer}>
-        <div ref={slider} className={styles.slider}>
-          <p ref={firstText}>Freelance Developer -</p>
-          <p ref={secondText}>Freelance Developer -</p>
-        </div>
-      </div>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+      <Landing />
     </main>
   );
 }
