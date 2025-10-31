30565b9d49f27a60114be18891662656ccb744dd
DrawingNea <lcy.li@outlook.de>
2025-10-31 11:09:02 +0100

    feat: add sticky mouse

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 import styles from './page.module.scss';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { AnimatePresence } from 'framer-motion';
 import Preloader from '../components/Preloader';
 import Landing from '../components/Landing';
@@ -8,9 +8,13 @@ import TextClip from '../components/TextClip';
 import TextParallax from '../components/TextParallax';
 import TextGradient from '../components/TextGradient';
 import Description from '../components/Description';
+import Header from '../components/Header';
+import StickyCursor from '../components/StickyCursor';
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
+
+  const stickyElement = useRef(null);
 
   useEffect(() => {
     (async () => {
@@ -30,6 +34,8 @@ export default function Home() {
       <AnimatePresence mode="wait">
         {isLoading && <Preloader />}
       </AnimatePresence>
+      <Header ref={stickyElement} />
+      <StickyCursor stickyElement={stickyElement} />
       <Landing />
       <TextClip />
       <TextParallax />
