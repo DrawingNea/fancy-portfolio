2a13951889038bc65c4aa994b4ac9d00a63e0ae7
DrawingNea <lcy.li@outlook.de>
2025-11-02 16:51:11 +0100

    feat: adjust footer links of header nav

--- src/components/Header/nav/Footer/index.jsx
+++ src/components/Header/nav/Footer/index.jsx
@@ -3,10 +3,31 @@ import styles from './style.module.scss';
 export default function index() {
   return (
     <div className={styles.footer}>
-      <a>Awwwards</a>
-      <a>Instagram</a>
-      <a>Dribble</a>
-      <a>LinkedIn</a>
+      <a
+        href="https://www.xing.com/profile/Evelyn_Lindner8"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Xing
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/evelyn-l-b83888231/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn
+      </a>
+
+      <a
+        href="https://github.com/DrawingNea"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
+
+      <a href="mailto:Evelyn.Lindner@outlook.com">Email Me</a>
     </div>
   );
 }
