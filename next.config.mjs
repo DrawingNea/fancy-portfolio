8c13b266ec407f92ac718c12e631d2a3013f1a99
DrawingNea <lcy.li@outlook.de>
2025-11-03 02:43:01 +0100

    feat: add next js config again

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+const repoName = '/fancy-portfolio';
+
+const nextConfig = {
+  basePath: repoName,
+  assetPrefix: repoName,
+  reactStrictMode: true,
+  output: 'export',
+};
+
+export default nextConfig;
