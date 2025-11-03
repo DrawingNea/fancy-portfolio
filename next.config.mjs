8fd343c92bcdcb9310394547959c09ea8a4f94d8
DrawingNea <lcy.li@outlook.de>
2025-11-03 12:38:39 +0100

    fix: adjust next config to also have asset prefixes for basepath on prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 // next.config.mjs
 const isProd = process.env.NODE_ENV === 'production';
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+  basePath: isProd ? basePath : '',
+  assetPrefix: isProd ? basePath : '',
   output: 'export',
   images: {
     unoptimized: true,
